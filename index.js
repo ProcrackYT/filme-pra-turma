d316dc0d1a7aeb03a7635a6341b9be3ed2cc92a3
ProcrackYT <84486489+ProcrackYT@users.noreply.github.com>
2021-08-11 10:30:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg = window.document.getElementById("msg")
+    var data = new Date()
+    var hora = data.getHours()
+
+
+
+    if (hora >= 0 && hora < 12){
+        document.body.style.background ='#e2cd9f'
+    }
+    else if (hora >= 12 && hora < 18){
+        document.body.style.background ='#b9846f'
+    }
+    else{
+        document.body.style.background ='#515154'
+    }
+}
